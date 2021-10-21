aaff8c28825deb3eeb344e3fa64a371a18c005c1
Klemen <klemenzmahar@gmail.com>
2021-10-21 18:35:21 +0200

    comit

--- src/jack/Main.java
+++ src/jack/Main.java
@@ -4,8 +4,6 @@ import java.io.IOException;
 
 public final class Main {
     public static void main(String[] args) {
-        //new window creation
-        /*new Window();*/
         //run code, if IO error is encountered print error log
         try {
             new JackManager();
