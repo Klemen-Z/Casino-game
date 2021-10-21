d1e6c7e2457d3e51b4a58f823a57bb29230494da
Klemen <klemenzmahar@gmail.com>
2021-10-21 18:57:34 +0200

    Changes by Bojan

--- src/jack/Main.java
+++ src/jack/Main.java
@@ -3,7 +3,16 @@ import java.io.IOException;
 import javax.sound.sampled.LineUnavailableException;
 import javax.sound.sampled.UnsupportedAudioFileException;
 public final class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws UnsupportedAudioFileException, IOException, LineUnavailableException{
+        //Fenster wird automatisch beim erstellen des Objektes erstellt
+
+        Frame frame = new Frame();
+
+        //Hintergrundmusik wird gestartet
+
+        Backgroundmusic music = new Backgroundmusic();
+        music.music();
+
         //run code, if IO error is encountered print error log
         try {
             new JackManager();
