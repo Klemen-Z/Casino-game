e63858e98b4f02fcb1d8de5b10427dd23147f5da
Klemen <klemenzmahar@gmail.com>
2021-11-12 16:26:30 +0100

    comit

--- src/main/Main.java
+++ src/main/Main.java
@@ -5,7 +5,6 @@ import javax.sound.sampled.UnsupportedAudioFileException;
 public final class Main {
     public static void main(String[] args) throws UnsupportedAudioFileException, IOException, LineUnavailableException{
         //Hintergrundmusik wird gestartet
-
         Backgroundmusic music = new Backgroundmusic();
         music.music();
         //run code, if IO error is encountered print error log
